f13f244d7a54f7e35999a72ee90dc4bb8e35c879
changhyeuk <changhyeuk@gmail.com>
2025-02-21 09:26:21 -0500

    simple case updated

--- IOS_GOC_File_Converter.py
+++ IOS_GOC_File_Converter.py
@@ -13,7 +13,7 @@ height = 2230
 # Define Dynamic Range
 D_Range_Object = (0,4095)
 D_Range_Raw = (0,4095)
-D_Range_1 = (0,2000)
+D_Range_1 = (0,100)
 D_Range_2 = (0,1000)
 D_Range_3 = (0,1500)
 D_Size = (1620, 2230)
@@ -36,4 +36,4 @@ for file in file_list:
         Source_file = image_tool.open_raw_image(target_file,height,width,1)
         #image_tool.save_simple_bmp(target_file[:-4],Source_file,D_Range_1)
         image_tool.save_simple_bmp(target_file[:-4],Source_file,D_Range_2)
-        image_tool.save_simple_bmp(target_file[:-4],Source_file,D_Range_3)
+        #image_tool.save_simple_bmp(target_file[:-4],Source_file,D_Range_3)
